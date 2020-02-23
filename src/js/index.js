993111113c1c35bc7d0379079076194cb0d579b3
Nikita <nikitka.kuznetsov.1994@mail.ru>
2020-02-23 03:23:44 +0300

    5 blocks ready

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,20 @@
 
-// $(document).ready(function() {
-//   console.log(1)
-// });
+$(document).ready(function() {
+    $(document).ready(function(){
+        $('.owl-carousel').owlCarousel({
+            items: 1,
+            loop: true,
+            nav: true
+        });
+    });
+
+    var clock;
+    clock = $('.clock').FlipClock({
+        clockFace: 'DailyCounter',
+        autoStart: false,
+        language: 'ru'
+    });
+    clock.setTime(220880);
+    clock.setCountdown(true);
+    clock.start();
+});
