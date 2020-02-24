5c1c7ace9d0dcad41ef7999cb4a5e1bb8f124fb3
Nikita <nikitka.kuznetsov.1994@mail.ru>
2020-02-25 00:46:40 +0300

    pay ready

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,44 @@
 
 $(document).ready(function() {
-    $(document).ready(function(){
-        $('.owl-carousel').owlCarousel({
-            items: 1,
-            loop: true,
-            nav: true
-        });
+    $('.js-join__slide').owlCarousel({
+        items: 1,
+        loop: true,
+        nav: true
+    });
+
+    $('.js-commit__slide').owlCarousel({
+        items: 1,
+        loop: true,
+        responsive : {
+            0 : {
+                nav: false,
+            },
+            481 : {
+                nav: true,
+            },
+        }
     });
 
     var clock;
+
     clock = $('.clock').FlipClock({
         clockFace: 'DailyCounter',
         autoStart: false,
+        countdown: true,
         language: 'ru'
     });
-    clock.setTime(220880);
-    clock.setCountdown(true);
-    clock.start();
+
+    function getSecondsToTomorrow() {
+        var now = new Date();
+        // завтрашняя дата
+        var tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 3);
+        var diff = tomorrow - now;
+        return Math.round(diff / 1000); // преобразуем в секунды
+    }
+
+    var $result = getSecondsToTomorrow();
+
+    clock.setTime($result);      //Устанавливаем нужное время в секундах
+    clock.setCountdown(true);   //Устанавливаем отсчет назад
+    clock.start();              //Запускаем отсчет
 });
